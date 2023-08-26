60b0570728c18e892d2eb2013a8cef5d9f9de02e
annimank <mankki.anni@gmail.com>
2023-08-26 12:02:57 +0300

    Second

--- App.js
+++ App.js
@@ -1,144 +1,52 @@
-/*
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
-
-export default function App() {
-
-  const [guess, setGuess] = useState("");
-
-  const button = () => {
-    guessInt= parseInt(guess);
-    rand = Math.floor(Math.random() * 100) + 1;
-    if (guessInt == rand) {
-      Alert.alert("Amazinggg");
-    } else if (guessInt > rand) {
-      Alert.alert("Too bigg");
-    } else {
-      Alert.alert("Too smalll");
-    }
-
-  };
-
-  return (
-    <View style={styles.container}>
-      <Text>Guessing game</Text>
-      <Text>Guess a number between 1 and 100</Text>
-      <TextInput style={styles.input} onChangeText={guess => setGuess(guess)} value={guess} keyboardType="numeric"/>
-      <Button onPress={button} title="Make a guess" />
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input: {
-    width: 200,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 10,
-  },
-});
-
-
-*/
-/*
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
-
-export default function App() {
-  const [guess, setGuess] = useState('');
-
-  const button = () => {
-    const guessInt = parseInt(guess);
-    const rand = Math.floor(Math.random() * 100) + 1;
-    if (guessInt === rand) {
-      Alert.alert('Amazinggg');
-    } else if (guessInt > rand) {
-      Alert.alert('Too bigg');
-    } else {
-      Alert.alert('Too smalll');
-    }
-  };
-
-  return (
-    <View style={styles.container}>
-      <Text>Guessing game</Text>
-      <Text>Guess a number between 1 and 100</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={guess => setGuess(guess)}
-        value={guess}
-        keyboardType="numeric"
-      />
-      <Button onPress={button} title="Make a guess" />
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input: {
-    width: 200,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 10,
-  },
-});
-*/
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
 
 export default function App() {
   const [guess, setGuess] = useState('');
   const [correctNumber, setCorrectNumber] = useState(null);
+  const [attempts, setAttempts] = useState(0);
+
+  useEffect(() => {
+    setCorrectNumber(Math.floor(Math.random() * 100) + 1);
+    setAttempts(1);
+  }, []);
 
   const button = () => {
     const guessInt = parseInt(guess);
-    const rand = Math.floor(Math.random() * 100) + 1;
-    setCorrectNumber(rand); // Save the correct number
+    setAttempts(attempts + 1);
 
-    if (guessInt === rand) {
-      Alert.alert('Amazinggg');
-    } else if (guessInt > rand) {
-      Alert.alert('Too bigg');
+    if (guessInt === correctNumber) {
+      Alert.alert(`Amazing! You guessed the correct number ${correctNumber} in ${attempts} attempts.`);
+      console.log('correct')
+    } else if (guessInt > correctNumber) {
+      Alert.alert('Too big');
+      console.log('big')
     } else {
-      Alert.alert('Too smalll');
+      Alert.alert('Too smal');
+      console.log('small')
     }
+ 
   };
 
   return (
     <View style={styles.container}>
-      <Text>Guessing game</Text>
-      <Text>Guess a number between 1 and 100</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={guess => setGuess(guess)}
-        value={guess}
-        keyboardType="numeric"
-      />
-      <Button onPress={button} title="Make a guess" />
-      
-      {/* Display the correct number when an incorrect guess is made */}
-      {correctNumber !== null && (
-        <Text>Correct number: {correctNumber}</Text>
-      )}
-      
+      <View style={styles.text}>
+        <Text style={styles.headerText}>Guessing game</Text>
+        <Text>Guess a number between 1 and 100</Text>
+      </View>
+      <View style={styles.input}>
+        <TextInput
+          style={styles.input}
+          onChangeText={guess => setGuess(guess)}
+          value={guess}
+          placeholder='Make a guess here'
+          keyboardType="numeric"
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button onPress={button} title="Guess" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -151,10 +59,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
   input: {
     width: 200,
     borderColor: 'gray',
-    borderWidth: 1,
+    borderWidth: 0,
     marginBottom: 10,
+    marginTop: 10,
+  },
+  buttonContainer: {
+    alignItems: 'center',
   },
 });
--- AppLaskin.js
+++ AppLaskin.js
@@ -23,11 +23,17 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>I can do addition and substraction</Text>
-      <TextInput style={{ width: 200, borderColor: 'gray', borderWidth: 1 }} onChangeText={textOne => setTextOne(textOne)} value={textOne} keyboardType="numeric"/>
-      <TextInput style={{ width: 200, borderColor: 'gray', borderWidth: 1 }} onChangeText={textTwo => setTextTwo(textTwo)} value={textTwo} keyboardType="numeric"/>
-      <Button onPress={buttonOne} title="+" />
-      <Button onPress={buttonTwo} title="-" />
+      <View style={styles.header}>
+        <Text>I can do addition and substraction</Text>
+      </View>
+      <View style={styles.textInput}>
+        <TextInput onChangeText={textOne => setTextOne(textOne)} value={textOne} placeholder="Enter number 1" keyboardType="numeric" />
+        <TextInput onChangeText={textTwo => setTextTwo(textTwo)} value={textTwo} placeholder="Enter number 2" keyboardType="numeric" />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button onPress={buttonOne} title="+" />
+        <Button onPress={buttonTwo} title="-" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -40,72 +46,26 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  input: {
-    width: 200,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 10,
-  },
-});
-
-
-/*
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
-
-export default function App() {
-  const [textOne, setTextOne] = useState("");
-  const [textTwo, setTextTwo] = useState("");
-
-  const buttonOne = () => {
-    const num1 = parseInt(textOne);
-    const num2 = parseInt(textTwo);
-    const result = num1 + num2;
-    Alert.alert("Result", result.toString());
-  };
-
-  const buttonTwo = () => {
-    const num1 = parseInt(textOne);
-    const num2 = parseInt(textTwo);
-    const result = num1 - num2;
-    Alert.alert("Result", result.toString());
-  };
-
-  return (
-    <View style={styles.container}>
-      <Text>I can do addition and subtraction</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={text => setTextOne(text)}
-        value={textOne}
-        keyboardType="numeric"
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={text => setTextTwo(text)}
-        value={textTwo}
-        keyboardType="numeric"
-      />
-      <Button onPress={buttonOne} title="+" />
-      <Button onPress={buttonTwo} title="-" />
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  header: {
+    backgroundColor: '#f0f0f0',
+    padding: 10,
+    marginBottom: 0,
+    marginTop: 20,
+    borderColor: '#ccc',
+    borderWidth: 4,
     alignItems: 'center',
-    justifyContent: 'center',
+    width: 220,
   },
-  input: {
-    width: 200,
+  textInput: {
+    width: 220,
     borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 10,
+    borderWidth: 4,
+    marginBottom: 20,
+    marginTop: 20,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: 60,
   },
 });
-*/
